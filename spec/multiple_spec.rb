102e2ab628474cf712d768e6d67715eeebe2f221
Le Lan-Toson <clelantoson@hotmail.fr>
2020-01-21 15:36:07 +0100

    added multiple_ spec.rb

---
+++ spec/multiple_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../lib/multiple.rb'
+
+describe "the is_multiple_of_3_or_5? method" do
+    it "should return TRUE when an integer is a multiple of 3 or 5" do
+      expect(is_multiple_of_3_or_5?(3)).to eq(true)
+      expect(is_multiple_of_3_or_5?(5)).to eq(true)
+      expect(is_multiple_of_3_or_5?(51)).to eq(true)
+      expect(is_multiple_of_3_or_5?(45)).to eq(true)
+    end
+  
+    it "should return FALSE when an integer is NOT a multiple of 3 or 5" do
+      # je te laisse écrire ces tests-là
+    end
+  end
